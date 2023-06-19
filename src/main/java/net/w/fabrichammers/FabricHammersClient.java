de80afd99367ec5a262432ea3a3e797858d9a4e5
bdani <bdani0717@gmail.com>
2023-06-19 09:41:09 +0200

    Initial commit

---
+++ src/main/java/net/w/fabrichammers/FabricHammersClient.java
@@ -0,0 +1,10 @@
+package net.w.fabrichammers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FabricHammersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/w/fabrichammers/FabricHammersDataGenerator.java
@@ -0,0 +1,11 @@
+package net.w.fabrichammers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FabricHammersDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
