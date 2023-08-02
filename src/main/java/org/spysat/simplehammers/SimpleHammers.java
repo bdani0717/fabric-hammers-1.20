d71bafc4b019d79e676dd8d708e230fa6e591f0c
Ashton Bennet <ashtonquinn@disroot.org>
2023-08-02 13:04:51 -0400

    added iron and stone hammers

--- src/main/java/org/spysat/simplehammers/SimpleHammers.java
+++ src/main/java/org/spysat/simplehammers/SimpleHammers.java
@@ -1,9 +1,10 @@
 package org.spysat.simplehammers;
 
 import net.fabricmc.api.ModInitializer;
+// import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+// import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ToolMaterials;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
@@ -11,11 +12,21 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class SimpleHammers implements ModInitializer {
-    public static final TagKey<Block> HAMMER_TAG = TagKey.of(RegistryKeys.BLOCK, new Identifier("simplehammers", "hammer_tag"));
+
+    //Eventually, i'd like to move all the definitions of in-game items to their own class file
+    //
+    public static final TagKey<Block> HAMMER_TAG = TagKey.of(RegistryKeys.BLOCK, new Identifier("simplehammers", "hammer_tag")); //Custom block tag
+
+    //Define items before initialisation
+    public static final HammerItem WOODEN_HAMMER = new HammerItem(1.5f, -3.3f, ToolMaterials.WOOD, HAMMER_TAG, new Item.Settings());
+    public static final HammerItem STONE_HAMMER = new HammerItem(1.5f, -3.3f, ToolMaterials.STONE, HAMMER_TAG, new Item.Settings());
+    public static final HammerItem IRON_HAMMER = new HammerItem(1.5f, -3.3f, ToolMaterials.IRON, HAMMER_TAG, new Item.Settings());
+
     @Override
     public void onInitialize() {
-        Registry.register(Registries.ITEM, new Identifier("simplehammers", "wooden_hammer"),
-                new HammerItem(1.0f, -3.1f, ToolMaterials.WOOD, HAMMER_TAG, new Item.Settings())
-        );
+        //Register Items
+        Registry.register(Registries.ITEM, new Identifier("simplehammers", "wooden_hammer"), WOODEN_HAMMER);
+        Registry.register(Registries.ITEM, new Identifier("simplehammers", "stone_hammer"), STONE_HAMMER);
+        Registry.register(Registries.ITEM, new Identifier("simplehammers", "iron_hammer"), IRON_HAMMER);
     }
 }
