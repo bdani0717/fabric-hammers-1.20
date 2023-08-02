f07d4a7ce71c7f610cce254cd8478f7a01a417bf
CanadianBacon <beamconnor@gmail.com>
2023-08-02 11:30:12 -0700

    Make hammer work :)

--- src/main/java/org/spysat/simplehammers/HammerItem.java
+++ src/main/java/org/spysat/simplehammers/HammerItem.java
@@ -2,14 +2,29 @@ package org.spysat.simplehammers;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.MiningToolItem;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HammerItem extends MiningToolItem {
+    public static HashMap<Block, Block> HammeringMap = new HashMap<>(
+            Map.of(
+                    Blocks.COBBLED_DEEPSLATE, Blocks.COBBLESTONE,
+                    Blocks.COBBLESTONE, Blocks.GRAVEL,
+                    Blocks.GRAVEL, Blocks.DIRT,
+                    Blocks.DIRT, Blocks.SAND
+            )
+    );
+
     public HammerItem(float attackDamage, float attackSpeed, ToolMaterial material, TagKey<Block> effectiveBlocks, Item.Settings settings) {
         super(attackDamage, attackSpeed, material, effectiveBlocks, settings);
     }
@@ -18,18 +33,23 @@ public class HammerItem extends MiningToolItem {
     public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
         return state.isIn(BlockTags.PICKAXE_MINEABLE) || state.isIn(BlockTags.SHOVEL_MINEABLE) ? this.miningSpeed : 1.0f;
     }
+
     @Override
     public boolean isSuitableFor(BlockState state) { //Check the tool material to compare whether it can be mined by the hammer
-        if (state.isIn(BlockTags.PICKAXE_MINEABLE) || state.isIn(BlockTags.SHOVEL_MINEABLE)) {
-            int i = this.getMaterial().getMiningLevel();
-            if (i < 3 && state.isIn(BlockTags.NEEDS_DIAMOND_TOOL)) {
-                return false;
-            } else if (i < 2 && state.isIn(BlockTags.NEEDS_IRON_TOOL)) {
-                return false;
-            } else {
-                return i < 1 && state.isIn(BlockTags.NEEDS_STONE_TOOL);
-            }
+        if (state.isIn(SimpleHammers.HAMMER_TAG)) {
+            return this.getMaterial().getMiningLevel() >= getMiningLevel(state);
         }
         return false;
+    }
+
+    public static int getMiningLevel(BlockState state) {
+        if (state.isIn(BlockTags.NEEDS_DIAMOND_TOOL)) {
+            return ToolMaterials.DIAMOND.getMiningLevel();
+        } else if (state.isIn(BlockTags.NEEDS_IRON_TOOL)) {
+            return ToolMaterials.IRON.getMiningLevel();
+        } else if (state.isIn(BlockTags.NEEDS_STONE_TOOL)) {
+            return ToolMaterials.STONE.getMiningLevel();
+        }
+        return 0;
     }
 }
