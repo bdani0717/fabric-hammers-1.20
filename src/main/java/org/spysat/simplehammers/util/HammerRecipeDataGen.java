a4807d6ff226b20a50e206a9e49b605271845f34
Ashton Bennet <123019691+ashtonqlb@users.noreply.github.com>
2023-08-19 16:45:26 -0400

    Delete src/main directory

--- src/main/java/org/spysat/simplehammers/util/HammerRecipeDataGen.java
+++
@@ -1,6 +0,0 @@
-package org.spysat.simplehammers.util;
-
-public class HammerRecipeDataGen {
-}
-
-//TODO - use the Fabric Data Generation API to manipulate the mineable_with_hammer.json file with entries from the HammeringMap
